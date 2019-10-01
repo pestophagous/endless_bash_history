70b79721fabc5d1ce3b9cb3a81f3fe3fdb9a837d
pestophagous <pestophagous@users.noreply.github.com>
2019-09-30 17:36:18 -0700

    version 0.0

---
+++ bin/install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "Hey, you should source this script, not execute it!"
+    exit 1
+fi
+
+SCRIPT_VER=0
+
+# check if ~/.persistent_history exists and is appendable
+touch ~/.persistent_history
+if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; return -1; fi
+
+echo '# endless_bash_history/bin/install.sh sanity check' > ~/.persistent_history
+if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; return -1; fi
+
+
+# check if PROMPT_COMMAND is present and has uuid
+if [ -z "$PROMPT_COMMAND" ]; then
+    # install it
+    tmpfile=$(mktemp /tmp/endless_bash_history_install.XXXXXXXX)
+    ls data/for_bashrc
+    if [ $? != 0 ]; then echo run as 'source bin/install.sh' from git repo root; return -1; fi
+    cp data/for_bashrc $tmpfile
+    if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; return -1; fi
+
+    # Personalize this to your liking:
+    host_unique_name=$((hostname && uuid -F SIV) | xargs echo -n | tr ' ' '-')
+    echo host_unique_name is [$host_unique_name]
+
+    sed -i "s/host_part=.hostname./host_part=$host_unique_name/"  $tmpfile
+    if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; return -1; fi
+    cat ~/.bashrc $tmpfile > ~/.bashrc.new
+    if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; return -1; fi
+    cp ~/.bashrc.new ~/.bashrc
+    if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; return -1; fi
+    rm ~/.bashrc.new
+    if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; return -1; fi
+    source ~/.bashrc
+else
+    # we have a prompt command.
+    # complain/fail if it does not contain uuid
+    found_our_uuid=$(type $PROMPT_COMMAND | grep c7056b84-e3d3-11e9-b9a3-37962ff2c46c | wc -l)
+    if [ "$found_our_uuid" != "1" ]; then
+        echo Cannot complete the install. Unknown PROMPT_COMMAND exists.
+        return -1
+    fi
+
+    echo Install script is happy with preexisting PROMPT_COMMAND
+fi
---
+++ bin/push_endless_bash.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SCRIPT_VER=0
+
+# is there a .persistent_history ?
+ls ~/.persistent_history > /dev/null
+if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; exit -1; fi
+
+timeprefix=$(date --iso-8601=minutes | tr ':' '_')
+
+tmpfile=$(mktemp "/tmp/${timeprefix}.persistent_history.XXXXXXXX")
+if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; exit -1; fi
+
+# is there a .persistent_history.trailing ?
+if [ -e ~/.persistent_history.trailing ]; then
+    # if so, then diff PH and PHT, put new content in timestamp_uuid
+    diff ~/.persistent_history ~/.persistent_history.trailing | cut -c3- > $tmpfile
+    if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; exit -1; fi
+else
+    # since we did not find the "trailing" log, we will back up the whole main log
+    cp ~/.persistent_history $tmpfile
+    if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; exit -1; fi
+fi
+
+# run pluggable function on file:
+endless_bash_pluggable_push $tmpfile
+if [ $? != 0 ]; then echo early termination at v$SCRIPT_VER:$LINENO; exit -1; fi
+
+# cp PH onto PHT
+cp ~/.persistent_history ~/.persistent_history.trailing
